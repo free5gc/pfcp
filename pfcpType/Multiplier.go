af3cfb3529a8d8ddcdbbb0345767bf0e4d8ea9ef
JJShen2000 <gimgim7910@gmail.com>
2022-10-25 10:29:59 +0800

    feat: Add Marshal for AggregatedURRID & Multiplier

--- pfcpType/Multiplier.go
+++ pfcpType/Multiplier.go
@@ -1,5 +1,17 @@
 package pfcpType
 
 type Multiplier struct {
-	Multiplierdata []byte
+	Multiplierdata []byte 
+}
+
+func (m *Multiplier) MarshalBinary() (data []byte, err error) {
+	data = m.Multiplierdata
+
+	return data, nil
+}
+
+func (m *Multiplier) UnmarshalBinary(data []byte) error {
+	m.Multiplierdata = data
+
+	return nil
 }
